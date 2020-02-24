ea98fbec03264adcad23d7a56223649f5ebea463
manpreet singh <manpreet.singh10196@gmail.com>
2020-02-24 16:21:06 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div >
+        
+        <p >
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
