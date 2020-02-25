6c2aea9d94e8e759b3acc13f969909e6988291fb
manpreet singh <manpreet.singh10196@gmail.com>
2020-02-25 08:53:12 +0530

    create layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
+import classes from './Layout.css';
 
 const layout = ( props ) => (
     <Aux>
         <div>Toolbar, Sidebar, Backdrop </div>
-       <main>
+       <main className={classes.Content}>
         {props.children}
         </main>
     </Aux>
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
 
 import Aux from '../../hoc/Aux';
+import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component{
     render(){
         return (
             <Aux>
-                <div>Burger</div>
+                <Burger />
                 <div>Build Controls</div>
             </Aux>
         );
