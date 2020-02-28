14edb623524dad3bce2620f3b237048a8ef44209
manpreet singh <manpreet.singh10196@gmail.com>
2020-02-28 09:22:17 +0530

    finished remove ingregients

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -5,7 +5,7 @@ import BuildControl from './BuildControl/BuildControl';
 
 const controls =[
     { label:'Salad', type: 'salad'},
-    { label:'bacon', type: 'becon'},
+    { label:'Bacon', type: 'bacon'},
     { label:'Cheese', type: 'cheese'},
     { label:'Meat', type: 'meat'}
 ];
@@ -13,7 +13,12 @@ const controls =[
 const buildControls =(props) => ( 
     <div className={classes.BuildControls}>
         {controls.map( ctrl => (
-            <BuildControl key={ctrl.label} label={ctrl.label} />
+            <BuildControl 
+            key={ctrl.label} 
+            label={ctrl.label} 
+            added={()=>props.ingredientAdded(ctrl.type)}
+            removed={()=>props.ingredientRemoved(ctrl.type)}
+            disabled={props.disabled[ctrl.type]}/>
         ))}
 
     </div>
