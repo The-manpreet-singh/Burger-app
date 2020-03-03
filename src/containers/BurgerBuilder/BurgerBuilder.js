71d10cda3eb92b01a933843378b393b4395f6af3
Manpreet Singh <manpreet.singh10196@gmail.com>
2020-03-03 16:05:28 +0530

    adding price

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -69,7 +69,8 @@ class BurgerBuilder extends Component {
                 <BuildControls 
                   ingredientAdded={this.addIngredientHandler} 
                   ingredientRemoved={this.removeIngredientHandler}
-                  disabled={disabledInfo} />
+                  disabled={disabledInfo}
+                  price={this.state.totalPrice} />
             </Aux>
         );
     }
