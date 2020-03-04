dd55a9ee41c569a5c3ed419598d5b778af5a1294
Manpreet Singh <manpreet.singh10196@gmail.com>
2020-03-04 10:06:14 +0530

    add logo

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -19,6 +19,7 @@
              <ul>
                 {ingredientSummary}
              </ul>
+     <p><storng>Total Price: {props.price.toFixed(2 )}</storng></p>
              <p>Continue to Checkout ?</p>
              <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
              <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -97,6 +97,7 @@ class BurgerBuilder extends Component {
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
                     <OrderSummary 
                     ingredients={this.state.ingredients}
+                    price={this.state.totalPrice}
                     purchaseCancelled={this.purchaseCancelHandler}
                     purchaseContinued={this.purchaseContinueHandler}/>
                 </Modal>
