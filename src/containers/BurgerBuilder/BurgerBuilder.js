19f5f51cc31c40fd65e9849bbe068cff2e198df9
Manpreet Singh <manpreet.singh10196@gmail.com>
2020-03-04 07:33:17 +0530

    add modal

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,7 +3,8 @@ import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
-
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 const INGREDIENT_PRICES ={
     salad:0.5,
     cheese:0.4,
@@ -80,6 +81,9 @@ class BurgerBuilder extends Component {
        //{salad: true, meat:false, ...}
         return (
             <Aux>
+                <Modal>
+                    <OrderSummary ingredients={this.state.ingredients}/>
+                </Modal>
                 <Burger ingredients={this.state.ingredients }/>
                 <BuildControls 
                   ingredientAdded={this.addIngredientHandler} 
