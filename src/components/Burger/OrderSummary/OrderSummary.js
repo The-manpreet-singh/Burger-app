72d11ae90255d6e2d58ac387bd7ce179ca24eaee
Manpreet Singh <manpreet.singh10196@gmail.com>
2020-03-10 16:44:13 +0530

    add lifecycle method

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,30 +1,37 @@
- import React from 'react';
+ import React, { Component } from 'react';
 
- import Aux from '../../../hoc/Aux';
+ import Aux from '../../../hoc/Aux1/Aux';
  import Button from '../../UI/Button/Button';
 
- const orderSummary =(props) => {
-     const ingredientSummary = Object.keys( props.ingredients )
-           .map( igKey => {
-                 return (
-                     <li key={igKey}>
-                         <span style={{ textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
-                     </li>
-                 )
-           });
-     return (
-         <Aux>
-             <h3>Your Order</h3>
-             <p> A delicious burger with the following ingredients:</p>
-             <ul>
-                {ingredientSummary}
-             </ul>
-     <p><storng>Total Price: {props.price.toFixed(2 )}</storng></p>
-             <p>Continue to Checkout ?</p>
-             <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-             <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-         </Aux>
-     );
+ class OrderSummary extends Component {
+     //this could be a functional component, doesn't have to be a class
+     componentDidUpdate() {
+         console.log('[OrderSummary] WillUpdate');
+     }
+     render () {
+        const ingredientSummary = Object.keys( this.props.ingredients )
+        .map( igKey => {
+              return (
+                  <li key={igKey}>
+                      <span style={{ textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
+                  </li>
+              )
+        });
+
+         return (
+            <Aux>
+            <h3>Your Order</h3>
+            <p> A delicious burger with the following ingredients:</p>
+            <ul>
+               {ingredientSummary}
+            </ul>
+    <p><storng>Total Price: {this.props.price.toFixed(2 )}</storng></p>
+            <p>Continue to Checkout ?</p>
+            <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+            <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+        </Aux>
+         );
+     }
  }
 
- export default orderSummary;
+ export default OrderSummary;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import Aux from '../../hoc/Aux';
+import Aux from '../../hoc/Aux1/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
