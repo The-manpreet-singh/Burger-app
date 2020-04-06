1885e3e6fdfda76169f222527cf9df7326930b0c
Manpreet Singh <manpreet.singh10196@gmail.com>
2020-04-06 13:42:42 +0530

    send post request into DB

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,9 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+
+import axios from '../../axios-orders';
+
 const INGREDIENT_PRICES ={
     salad:0.5,
     cheese:0.4,
@@ -81,7 +84,25 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler =()=> {
-       alert('You continue');
+       //alert('You continue');
+       const order = {
+           ingredients: this.state.ingredients,
+           price: this.state.totalPrice,
+           customer : {
+               name: 'Max Singh',
+               address: {
+                   street: 'Bangalore',
+                   zipCode: '411351',
+                   country: 'Germany'
+               },
+               email:'test@test.com'
+           },
+           deliveryMethod: 'fastest'
+
+       }
+       axios.post('/orders.json', order)
+       .then(response => console.log(response))
+       .catch(error => console.log(error));
     }
 
     render(){
