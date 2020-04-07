05c65d588611dd1a385568b76d6201931af035a0
Manpreet Singh <manpreet.singh10196@gmail.com>
2020-04-07 14:08:56 +0530

    remove interceptors

--- src/App.js
+++ src/App.js
@@ -4,10 +4,19 @@ import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
+  /* state= {
+    show : true
+  };
+ componentDidMount() {
+    setTimeout(()=>{
+       this.setState({show : false});
+    }, 5000);
+ } */
   render() {
     return (
       <div>
        <Layout>
+         {/* {this.state.show ? <BurgerBuilder /> : null} this is for testing*/}
          <BurgerBuilder />
        </Layout>
       </div>
--- src/hoc/withErrorHandler/withErrorhandler.js
+++ src/hoc/withErrorHandler/withErrorhandler.js
@@ -9,15 +9,21 @@ const withErrorHandler =(WrappedComponent, axios) => {
                     error: null 
                 }
                 componentWillMount () {
-                    axios.interceptors.request.use(req => {
+                    this.reqInterceptor = axios.interceptors.request.use(req => {
                         this.setState({error:null});
                         return req;
                 });
-                    axios.interceptors.response.use(res => res, error => {
+                this.resInterceptor =axios.interceptors.response.use(res => res, error => {
                             this.setState({error:error});
                     });
 
                 }
+
+                componentWillUnmount(){
+                   // console.log('Will Unmount', this.reqInterceptor, this.resInterceptor);
+                    axios.interceptors.request.eject(this.reqInterceptor);
+                    axios.interceptors.response.eject(this.resInterceptor);
+                 }
 
                 errorConfiremedHandler = () => {
                     this.setState({error : null})
