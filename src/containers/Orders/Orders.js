c4ce5a7ebf493d9cb52ead8725820dafcbd9d70d
Manpreet Singh <manpreet.singh10196@gmail.com>
2020-04-14 11:53:01 +0530

    fetch order from backend

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,15 +1,48 @@
-import React, {Component} from 'react';
+import React, {
+    Component
+} from 'react';
 import Order from '../../components/Order/Order';
+import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorhandler';
 
 class Orders extends Component {
-    render () {
-        return (
-            <div>
-                <Order />
-                <Order />
-            </div>
+    state = {
+        orders: [],
+        loading: true
+    }
+    componentDidMount() {
+        axios.get('/orders.json')
+            .then(res => {
+                //console.log(res.data); 
+                const fetchedOrders = [];
+                for (let key in res.data) {
+                    fetchedOrders.push({
+                        ...res.data[key],
+                        id: key
+                    });
+                }
+                this.setState({
+                    loading: false,
+                    orders: fetchedOrders
+                })
+            })
+            .catch(err => {
+                this.setState({
+                    loading: false
+                })
+            })
+    }
+    render() {
+        return ( <
+            div >
+            <
+            Order / >
+            <
+            Order / >
+            <
+            /div>
         );
     }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
