a5b04670668bed8aaa70e4ae2fbc90c1913d290c
Manpreet Singh <manpreet.singh10196@gmail.com>
2020-04-15 10:31:35 +0530

    adding dropdown

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -4,15 +4,39 @@ import classes from './Input.css';
 const input = (props) => {
     let inputElement =null;
 
-    switch (props.inputtype) {
+    switch (props.elementType) {
         case ('input'):
-            inputElement =<input className={classes.InputElement} {...props} />;
+            inputElement =<input 
+                  className={classes.InputElement} 
+                  {...props.elementConfig}
+                   value={props.value} />;
             break;
         case ('textarea'):
-            inputElement = <textarea className={classes.InputElement} {...props} />;
+            inputElement = <textarea
+                 className={classes.InputElement} 
+                 {...props.elementConfig}
+                 value={props.value} />;
             break;
+            case ('select'):
+                inputElement = (
+                <select
+                     className={classes.InputElement} 
+                     value={props.value}>
+                     {props.elementConfig.options.map(option => (
+                         <option 
+                            key={option.value}
+                            value={option.value}>
+                            {option.displayValue}
+                         </option>
+                     ))} 
+                     </select>
+                     );
+                break;   
          default:
-             inputElement= <input className={classes.InputElement} {...props} />;       
+             inputElement= <input 
+             className={classes.InputElement} 
+             {...props.elementConfig} 
+             value={props.value} />;       
     }
     
     return (
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -8,11 +8,58 @@ import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
     state = {
-        name: '',
-        email:'',
-        address: {
-            street: '',
-            postalCode:''
+        orderForm : {
+                name:{
+                    elementType: 'input',
+                    elementConfig: {
+                        type: 'text',
+                        placeholder: 'Your Name'
+                    },
+                    value: ''
+                },
+                street:{
+                    elementType: 'input',
+                    elementConfig: {
+                        type: 'text',
+                        placeholder: 'Street'
+                    },
+                    value: ''
+                },
+                zipCode: {
+                    elementType: 'input',
+                    elementConfig: {
+                        type: 'text',
+                        placeholder: 'ZIP Code'
+                    },
+                    value: ''
+                },
+                country: {
+                        elementType: 'input',
+                        elementConfig: {
+                            type: 'text',
+                            placeholder: 'Country'
+                        },
+                        value: ''
+                    },
+                email:{
+                    elementType: 'input',
+                    elementConfig: {
+                        type: 'email',
+                        placeholder: 'Your E-mail'
+                    },
+                    value: ''
+                },
+               deliveryMethod: {
+                elementType: 'select',
+                elementConfig: {
+                    options: [
+                        {value: 'fastest', displayValue:'Fastest' },
+                        {value: 'cheapest', displayValue:'Cheapest' }
+                    ]
+                },
+                value: ''
+            }
+ 
         },
         loading:false
     }
@@ -26,17 +73,7 @@ class ContactData extends Component {
        const order = {
            ingredients: this.props.ingredients,
            price: this.props.price,
-           customer : {
-               name: 'Max Singh',
-               address: {
-                   street: 'Bangalore',
-                   zipCode: '411351',
-                   country: 'Germany'
-               },
-               email:'test@test.com'
-           },
-           deliveryMethod: 'fastest'
-
+          
        }
        axios.post('/orders.json', order)
        .then(response => {
@@ -49,12 +86,24 @@ class ContactData extends Component {
     }
 
   render () {
+      
+    const formElementsArray= [];
+    for (let key in this.state.orderForm){
+        formElementsArray.push({
+            id: key,
+            config: this.state.orderForm[key]
+        });
+    }
+
       let form = (
-        <form>
-        <Input inputtype="input" type="text" name="name" placeholder="Your Name" />
-        <Input inputtype="input" type="email" name="email" placeholder="Your Email" />
-        <Input inputtype="input" type="text" name="street" placeholder="Your Street" />
-        <Input inputtype="input" type="text" name="postal" placeholder="Your Postal Code" />
+        <form> 
+        {formElementsArray.map(formElement => (
+            <Input 
+              key={formElement.id}
+              elementType={formElement.config.elementType}
+              elementConfig={formElement.config.elementConfig}
+              value={formElement.config.value} />
+        ))}
         <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
     </form>
       );
