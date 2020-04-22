b8853d18660563153ec172c1cf2931b85373f0fd
Manpreet Singh <manpreet.singh10196@gmail.com>
2020-04-22 13:57:44 +0530

    update order button

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -27,7 +27,7 @@ class BurgerBuilder extends Component {
   state = {
     // ingredients:null,
    // totalPrice: 4,
-    purchasable: false,
+   // purchasable: false,
     purchasing: false,
     loading: false,
     error: false,
@@ -52,7 +52,7 @@ class BurgerBuilder extends Component {
       .reduce((sum, el) => {
         return sum + el;
       }, 0);
-    this.setState({ purchasable: sum > 0 });
+     return sum > 0 ;
   }
 
   /* addIngredientHandler =( type ) =>{
@@ -138,7 +138,7 @@ class BurgerBuilder extends Component {
             ingredientAdded={this.props.onIngredientAdded}
             ingredientRemoved={this.props.onIngredientRemoved}
             disabled={disabledInfo}
-            purchasable={this.state.purchasable}
+            purchasable={this.updatePurchaseState(this.props.ings)}
             ordered={this.purchaseHandler}
             price={this.props.price}
           />
