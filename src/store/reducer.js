8e19a90eebc9a5683e81bc47e560b52a195a0200
Manpreet Singh <manpreet.singh10196@gmail.com>
2020-04-22 11:03:00 +0530

    add redux with reducer and action

---
+++ src/store/reducer.js
@@ -0,0 +1,13 @@
+import * as actionTypes from './actions';
+
+const initialState= {
+    ingredients:null,
+    totalPrice:4,
+}
+
+
+const reducer =(state= initialState, action) => {
+
+}
+
+export default reducer;
