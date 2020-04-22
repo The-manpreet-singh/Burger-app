2bf34f33da2ffd9374862707d55cb7c8319fff07
Manpreet Singh <manpreet.singh10196@gmail.com>
2020-04-22 11:24:00 +0530

    ingredient state with redux

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -32,13 +32,13 @@ class BurgerBuilder extends Component {
 
       componentDidMount () {
           console.log(this.props);
-          axios.get('https://react-burger-app-90ca7.firebaseio.com/ingredients.json ')
-            .then(response =>{
-                this.setState({ ingredients: response.data})
-            })
-            .catch(error => {
-                this.setState({error: true})
-            })
+        //   axios.get('https://react-burger-app-90ca7.firebaseio.com/ingredients.json ')
+        //     .then(response =>{
+        //         this.setState({ ingredients: response.data})
+        //     })
+        //     .catch(error => {
+        //         this.setState({error: true})
+        //     })
       }
 
     updatePurchaseState (ingredients) {
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,12 +1,35 @@
 import * as actionTypes from './actions';
 
 const initialState= {
-    ingredients:null,
+    ingredients: {
+        cheese:0,
+        bacon:0,
+        meat:0,
+        salad:0
+    },
     totalPrice:4,
 }
 
 
 const reducer =(state= initialState, action) => {
+    switch (action.type) {
+        case actionTypes.ADD_INGREDIENT: 
+          return {
+               ...state,
+               ingredients: {
+                   [action.ingredientName]: state.ingredients[action.ingredientName] + 1
+               }
+          }
+         case actionTypes.REMOVE_INGREDIENT:
+             return {
+                ...state,
+                ingredients: {
+                    [action.ingredientName]: state.ingredients[action.ingredientName] - 1
+                }
+             } 
+          default:
+              return state; 
+    }
 
 }
 
