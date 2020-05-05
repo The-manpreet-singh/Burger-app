763e260c9ca9cf47055fe0f3b9f1568765a0a1e5
manpreet-singh10196 <manpreet.singh10196@gmail.com>
2020-05-05 18:08:21 +0530

    add utitlity file for refactoring reducers

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject} from '../utility';
 
 const initialState = {
     orders : [],
@@ -6,52 +7,51 @@ const initialState = {
     purchased:false
 };
 
+const purchaseInit =(state,action)=> {
+    return updateObject( state, {purchased:false});
+};
+
+const purchaseBurgerStart =(state,action) => {
+    const newOrder = updateObject(action.orderData,{ id: action.orderId });
+    return updateObject(state,{
+        loading: false,
+        orders: state.orders.concat(newOrder),
+        purchased:true
+    });
+};
+const purchaseBurgerSuccess=(state,action) => {
+    return updateObject(state,{ loading:false});
+};
+
+const purchaseBurgerFail =(state,action) => {
+    return updateObject(state,{loading:false}); 
+};
+
+const fetchOrdersStart =(state,action) => {
+    return updateObject(state, { loading:true});  
+};
+
+const fetchOrdersSuccess =(state,action) => {
+    return updateObject(state, { 
+        orders: action.orders,
+        loading:false
+    }); 
+};
+
+const fetchOrdersFail =(state,action) => {
+    return updateObject(state,{ loading:false}); 
+};
+
 const reducer = (state= initialState, action) => {
     switch(action.type) {
-        case actionTypes.PURCHASE_INIT:
-            return {
-               ...state,
-               purchased:false
-            }
-        case actionTypes.PURCHASE_BURDER_START:
-            return {
-                ...state,
-                loading:true
-            }
-        case actionTypes.PURCHASE_BURGER_SUCCESS:
-            const newOrder = {
-                ...action.orderData,
-                id: action.orderId
-            }
-            return {
-               ...state,
-               loading: false,
-               orders: state.orders.concat(newOrder),
-               purchased:true
-            };
-        case actionTypes.PURCHASE_BURGER_FAIL:
-            return {
-                ...state,
-                loading:false
-            };   
-        case actionTypes.FETCH_ORDERS_START:
-                return {
-                    ...state,
-                    loading:true
-                }; 
-        case actionTypes.FETCH_ORDERS_SUCCESS:
-                    return {
-                        ...state,
-                        orders: action.orders,
-                        loading:false
-                    }; 
-        case actionTypes.FETCH_ORDERS_FAIL:
-                        return {
-                            ...state,
-                            loading:false
-                        };                        
-        default:
-            return state;     
+        case actionTypes.PURCHASE_INIT:return purchaseInit(state,action);
+        case actionTypes.PURCHASE_BURDER_START: return purchaseBurgerStart(state,action);
+        case actionTypes.PURCHASE_BURGER_SUCCESS: return purchaseBurgerSuccess(state,action);
+        case actionTypes.PURCHASE_BURGER_FAIL: return purchaseBurgerFail(state,action);
+        case actionTypes.FETCH_ORDERS_START: return fetchOrdersStart(state,action);       
+        case actionTypes.FETCH_ORDERS_SUCCESS: return fetchOrdersSuccess(state,action);       
+        case actionTypes.FETCH_ORDERS_FAIL: return fetchOrdersFail(state,action);
+        default:return state;     
     }
 };
 
