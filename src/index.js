e0491258d4bf53abed45cecf79b95168da06827c
manpreet-singh10196 <manpreet.singh10196@gmail.com>
2020-05-06 16:27:27 +0530

    storing token

--- src/index.js
+++ src/index.js
@@ -13,11 +13,14 @@ import burgerBuilderReducer from './store/reducers/burgerBuilder';
 import thunk from 'redux-thunk';
 import orderReducer from './store/reducers/order';
 
+import authReducer from './store/reducers/auth';
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const rootReducer = combineReducers({
   burgerBuilder: burgerBuilderReducer,
-  order:orderReducer
+  order:orderReducer,
+  auth:authReducer
 })
 
 const store= createStore( rootReducer, composeEnhancers(applyMiddleware(thunk) ) );
