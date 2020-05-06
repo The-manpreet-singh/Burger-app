e04b2d3385b27001dec4e6da1f554446df2813c9
manpreet-singh10196 <manpreet.singh10196@gmail.com>
2020-05-06 16:57:31 +0530

    add spinner

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -40,8 +40,7 @@ export const auth = (email, password, isSignup) => {
              dispatch(authSuccess(response.data.idToken, response.data.localId));
          })
          .catch(err => {
-             console.log(err);
-             dispatch(authFail(err));
+             dispatch(authFail(err.response.data.error));
          })
     }
 }
