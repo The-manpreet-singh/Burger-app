85249af324229af4ebd29ed8132b3adf17cf5db0
manpreet-singh10196 <manpreet.singh10196@gmail.com>
2020-05-06 08:18:52 +0530

    check validation

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -13,92 +13,90 @@ import * as actions from '../../../store/actions/index';
 
 class ContactData extends Component {
     state = {
-        orderForm : {
-
-                name:{
-                    elementType: 'input',
-                    elementConfig: {
-                        type: 'text',
-                        placeholder: 'Your Name'
-                    },
-                    value: '',
-                    validation : {
-                        required:true
-                    },
-                    valid: false,
-                    touched:false
+        orderForm: {
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Name'
                 },
-                street:{
-                    elementType: 'input',
-                    elementConfig: {
-                        type: 'text',
-                        placeholder: 'Street'
-                    },
-                    value: '',
-                    validation : {
-                        required:true
-                    },
-                    valid: false,
-                    touched:false
+                value: '',
+                validation: {
+                    required: true
                 },
-                zipCode: {
-                    elementType: 'input',
-                    elementConfig: {
-                        type: 'text',
-                        placeholder: 'ZIP Code'
-                    },
-                    value: '',
-                    validation : {
-                        required:true,
-                        minLength:5,
-                        maxLength:5,
-                        isNumaric:true
-                    },
-                    valid: false,
-                    touched:false
+                valid: false,
+                touched: false
+            },
+            street: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Street'
                 },
-                country: {
-                        elementType: 'input',
-                        elementConfig: {
-                            type: 'text',
-                            placeholder: 'Country'
-                        },
-                        value: '',
-                        validation : {
-                            required:true
-                        },
-                        valid: false,
-                        touched:false
-                    },
-                email:{
-                    elementType: 'input',
-                    elementConfig: {
-                        type: 'email',
-                        placeholder: 'Your E-mail'
-                    },
-                    value: '',
-                    validation : {
-                        required:true,
-                        isEmail:true
-                    },
-                    valid: false,
-                    touched:false
+                value: '',
+                validation: {
+                    required: true
                 },
-               deliveryMethod: {
+                valid: false,
+                touched: false
+            },
+            zipCode: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'ZIP Code'
+                },
+                value: '',
+                validation: {
+                    required: true,
+                    minLength: 5,
+                    maxLength: 5,
+                    isNumeric: true
+                },
+                valid: false,
+                touched: false
+            },
+            country: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Country'
+                },
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false,
+                touched: false
+            },
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Your E-Mail'
+                },
+                value: '',
+                validation: {
+                    required: true,
+                    isEmail: true
+                },
+                valid: false,
+                touched: false
+            },
+            deliveryMethod: {
                 elementType: 'select',
                 elementConfig: {
                     options: [
-                        {value: 'fastest', displayValue:'Fastest' },
-                        {value: 'cheapest', displayValue:'Cheapest' }
+                        {value: 'fastest', displayValue: 'Fastest'},
+                        {value: 'cheapest', displayValue: 'Cheapest'}
                     ]
                 },
                 value: 'fastest',
-                validation:{},
-                valid:true
+                validation: {},
+                valid: true
             }
- 
         },
-        formIsValid:false
+        formIsValid: false
     }
 
     orderHandler = (event) => {
@@ -122,29 +120,29 @@ class ContactData extends Component {
 
     checkValidity(value, rules) {
         let isValid = true;
-        if(!rules){
-          return true;
+        if (!rules) {
+            return true;
+        }
+        
+        if (rules.required) {
+            isValid = value.trim() !== '' && isValid;
         }
 
-        if(rules.required) {
-            isValid = value.trim() !== '' && isValid ;
+        if (rules.minLength) {
+            isValid = value.length >= rules.minLength && isValid
         }
 
-        if(rules.minLength) {
-            isValid =value.length >= rules.minLength && isValid ;
+        if (rules.maxLength) {
+            isValid = value.length <= rules.maxLength && isValid
         }
 
-        if(rules.maxLength) {
-            isValid =value.length <= rules.maxLength  && isValid ;
-        }
-
-        if(rules.isEmail){
-            const pattern= /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,15}/;
+        if (rules.isEmail) {
+            const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
             isValid = pattern.test(value) && isValid
         }
 
-        if(rules.isNumaric){
-            const pattern= /^\d+$/;
+        if (rules.isNumeric) {
+            const pattern = /^\d+$/;
             isValid = pattern.test(value) && isValid
         }
 
