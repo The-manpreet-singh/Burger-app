6624f1183f68937f75bd0401a6a1d558a83bcabe
manpreet-singh10196 <manpreet.singh10196@gmail.com>
2020-05-10 10:52:27 +0530

    testing component correctly

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -9,8 +9,24 @@ import NavigationItem from './NavigationItem/NavigationItem';
 configure({adapter: new Adapter()});
 
 describe('<NavigationItems />', ()=>{
+    let wrapper;
+    
+    beforeEach(()=>{
+        wrapper = shallow(<NavigationItems />);
+    });
+
     it('should render two <NavigationItem /> elements if not authenticated', ()=> {
-      const wrapper = shallow(<NavigationItems />);
+      
       expect(wrapper.find(NavigationItem)).toHaveLength(2);
-    })
+    });
+    it('should render three <NavigationItem /> elements if authenticated', ()=> {
+        //wrapper = shallow(<NavigationItems isAuthenticated />);
+        wrapper.setProps({isAuthenticated: true});
+        expect(wrapper.find(NavigationItem)).toHaveLength(3);
+      });
+      it('should an exact logout button', ()=> {
+        //wrapper = shallow(<NavigationItems isAuthenticated />);
+        wrapper.setProps({isAuthenticated: true});
+        expect(wrapper.contains(<NavigationItem link="/logout" >Logout</NavigationItem>)).toEqual(true)
+      });
 })
